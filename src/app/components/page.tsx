dc9bb4fb65dc517cd917fb52affc0f65de96c915
Scalco7 <fscalco7@gmail.com>
2024-07-18 17:32:15 -0300

    price tag

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import CartCount from "@/components/atoms/cartCount/cartCount";
+import PriceTag from "@/components/atoms/priceTag/priceTag";
 
 export default function Components() {
   return (
@@ -11,29 +12,65 @@ export default function Components() {
           width: "100%",
           height: "100%",
           display: "flex",
+          flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
           gap: "20px",
+          padding: "50px",
         }}
       >
-        <CartCount
-          quantity={0}
-          onClick={() => {
-            console.log("clicou");
+        <div
+          style={{
+            background: "black",
+            padding: "20px",
+            display: "flex",
+            gap: "20px",
           }}
-        />
-        <CartCount
-          quantity={5}
-          onClick={() => {
-            console.log("clicou");
+        >
+          <CartCount
+            quantity={0}
+            onClick={() => {
+              console.log("clicou");
+            }}
+          />
+          <CartCount
+            quantity={5}
+            onClick={() => {
+              console.log("clicou");
+            }}
+          />
+          <CartCount
+            quantity={10}
+            onClick={() => {
+              console.log("clicou");
+            }}
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            gap: "20px",
           }}
-        />
-        <CartCount
-          quantity={10}
-          onClick={() => {
-            console.log("clicou");
-          }}
-        />
+        >
+          <PriceTag
+            price={79.9}
+            onClick={() => {
+              console.log("quero comprar");
+            }}
+          />
+          <PriceTag
+            price={29.9}
+            onClick={() => {
+              console.log("quero comprar");
+            }}
+          />
+          <PriceTag
+            price={109.9}
+            onClick={() => {
+              console.log("quero comprar");
+            }}
+          />
+        </div>
       </div>
     </main>
   );
---
+++ src/components/atoms/priceTag/priceTag.tsx
@@ -0,0 +1,48 @@
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+import { useState } from "react";
+import styles from "./styles.module.css";
+
+interface PriceTagProps {
+  price: number;
+  onClick: () => void;
+}
+
+const poppins = Poppins({
+  weight: "500",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export default function PriceTag({ price, onClick }: PriceTagProps) {
+  const [isHover, setIsHover] = useState(false);
+
+  function handleOnMouseEnter() {
+    setIsHover(true);
+  }
+
+  function handleOnMouseLeave() {
+    setIsHover(false);
+  }
+
+  return (
+    <main
+      className={`${poppins.className} ${styles.main}`}
+      onClick={onClick}
+      onMouseEnter={handleOnMouseEnter}
+      onMouseLeave={handleOnMouseLeave}
+    >
+      {isHover ? (
+        <Image
+          src={"/cart.svg"}
+          alt={"Carrinho"}
+          width={24}
+          height={24}
+          priority
+        />
+      ) : (
+        <p>R$ {price.toFixed(2)}</p>
+      )}
+    </main>
+  );
+}
