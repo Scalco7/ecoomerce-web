85c2cb0f1b101ea2b07cec5fb7a02220f2dacaaa
Scalco7 <fscalco7@gmail.com>
2024-07-29 12:49:57 -0300

    home first section

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,20 +2,37 @@
 
 import Footer from "@/components/molecules/footer/footer";
 import HeaderNavigation from "@/components/molecules/headerNavigation/headerNavigation";
+import Image from "next/image";
+import styles from "./style.module.css";
+import { Anton } from "next/font/google";
+
+const anton = Anton({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Home() {
   return (
     <main>
-      <div style={{ width: "100%" }}>
+      <header className={styles.header}>
         <HeaderNavigation />
-      </div>
+      </header>
+      <section className={styles.firstSection}>
+        <Image src={"/basquete-back-home.png"} alt="background" fill={true} />
+        <section className={styles.firstTextBox}>
+          <p className={`${styles.fistFirstText} ${anton.className}`}>
+            Banana Monkey's
+          </p>
+          <p className={`${styles.fistSecondaryText}`}>Descubra a força</p>
+        </section>
+      </section>
       <br />
       <br />
       <br />
       <br />
-      <div style={{ width: "100%" }}>
-        <Footer />
-      </div>
+
+      <Footer />
     </main>
   );
 }
