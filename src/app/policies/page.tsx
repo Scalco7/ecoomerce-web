896def2e123cef1edc598c22b5231fe1710ef6f0
Scalco7 <fscalco7@gmail.com>
2024-08-05 11:04:05 -0300

    policies page

--- src/app/policies/page.tsx
+++ src/app/policies/page.tsx
@@ -3,20 +3,76 @@
 import HeaderNavigation from "@/components/molecules/headerNavigation/headerNavigation";
 import styles from "./style.module.css";
 import { useSearchParams } from "next/navigation";
+import { Zilla_Slab } from "next/font/google";
+import { useState } from "react";
+
+const zillaSlab = Zilla_Slab({
+  weight: "700",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Policies() {
   const searchParams = useSearchParams();
-  const section = searchParams.get("section");
+  const [section, setSection] = useState(
+    searchParams.get("section") ?? "privacy"
+  );
 
-  console.log(section);
+  const sectionMap: { [key: string]: any } = {
+    delivery: <main>delivery</main>,
+    exchanges: <main>exchanges</main>,
+    privacy: <main>privacy</main>,
+    term: <main>term</main>,
+  };
+
+  function handleChangeSection(sectionSelected: string) {
+    setSection(sectionSelected);
+  }
 
   return (
     <main className={styles.main}>
       <header className={styles.header}>
         <HeaderNavigation />
       </header>
-      <section className={`${styles.body}`}>
+      <section className={`${styles.body} ${zillaSlab.className}`}>
         <p className={styles.title}>Políticas do Site</p>
+        <section className={styles.dataSection}>
+          <nav className={styles.navBar}>
+            <section
+              className={`${styles.navTextBox} ${styles.borderRight} ${
+                section == "delivery" ? styles.selected : null
+              }`}
+              onClick={() => handleChangeSection("delivery")}
+            >
+              Política de Entrega
+            </section>
+            <section
+              className={`${styles.navTextBox} ${styles.borderRight} ${
+                section == "exchanges" ? styles.selected : null
+              }`}
+              onClick={() => handleChangeSection("exchanges")}
+            >
+              Política de Trocas e Devoluções
+            </section>
+            <section
+              className={`${styles.navTextBox} ${styles.borderRight} ${
+                section == "term" ? styles.selected : null
+              }`}
+              onClick={() => handleChangeSection("term")}
+            >
+              Política de Prazo e Formas de Entrega
+            </section>
+            <section
+              className={`${styles.navTextBox} ${
+                section == "privacy" ? styles.selected : null
+              }`}
+              onClick={() => handleChangeSection("privacy")}
+            >
+              Política de Privacidade
+            </section>
+          </nav>
+          {sectionMap[section]}
+        </section>
       </section>
     </main>
   );
