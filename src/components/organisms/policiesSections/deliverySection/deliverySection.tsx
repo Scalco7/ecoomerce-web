12b8818a10d1e699a1a554e8d842511adf2f1e27
Scalco7 <fscalco7@gmail.com>
2024-08-14 14:14:10 -0300

    políticas de entrega

--- src/components/organisms/policiesSections/deliverySection/deliverySection.tsx
+++ src/components/organisms/policiesSections/deliverySection/deliverySection.tsx
@@ -10,17 +10,36 @@ const zillaSlab = Zilla_Slab({
 export default function DeliverySection() {
   return (
     <main className={styles.main}>
-      <p className={styles.title}>Política de Entrega</p>
-      <section className={`${styles.boxText} ${zillaSlab.className}`}>
-        <p>
-          Entrega Entrega Entrega Entrega, Entrega Entrega Entrega Entrega
-          Entrega Entrega Entrega Entrega Entrega Entrega Entrega Entrega
-          Entrega Entrega Entrega Entrega Entrega Entrega Entrega EntregaEntrega
-          Entrega Entrega EntregaEntrega Entrega Entrega Entrega Entrega Entrega
-          Entrega Entrega Entrega Entrega Entrega EntregaEntrega Entrega Entrega
-          EntregaEntrega Entrega Entrega EntregaEntrega Entrega Entrega Entrega
-        </p>
-      </section>
+      <main className={styles.main}>
+        <p className={styles.title}>Políticas de Entrega</p>
+        <section className={styles.boxText}>
+          <section>
+            <p>1. Condições de Entrega</p>
+            <p className={zillaSlab.className}>
+              - A Banana Monkeys realiza entregas em todo o território nacional,
+              utilizando serviços de correios ou transportadoras parceiras.
+              <br /> - O prazo de entrega pode variar conforme a localidade do
+              destinatário e a forma de envio escolhida.
+            </p>
+          </section>
+          <section>
+            <p>2. Custos de Entrega</p>
+            <p className={zillaSlab.className}>
+              O custo de entrega será calculado no momento da finalização da
+              compra, com base no CEP informado e no peso/volume dos produtos.
+            </p>
+          </section>
+          <section>
+            <p>3. Tentativas de Entrega</p>
+            <p className={zillaSlab.className}>
+              Serão feitas até três tentativas de entrega. Caso não seja
+              possível realizar a entrega, o produto será retornado à Banana
+              Monkeys, e o cliente será responsável pelos custos de um novo
+              envio.
+            </p>
+          </section>
+        </section>
+      </main>
     </main>
   );
 }
