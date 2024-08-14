b2fe209fa1e5d083d18c7b51707c0a1f29e6387e
Scalco7 <fscalco7@gmail.com>
2024-08-14 15:10:20 -0300

    home presentation section responsividade

--- src/components/organisms/homePageSections/presentationSection/presentationSection.tsx
+++ src/components/organisms/homePageSections/presentationSection/presentationSection.tsx
@@ -1,6 +1,7 @@
-import { Anton } from "next/font/google";
+import { Anton, Zilla_Slab_Highlight } from "next/font/google";
 import styles from "./style.module.css";
 import Image from "next/image";
+import { useClientMediaQuery } from "@/hooks/useClientMediaQuery";
 
 const anton = Anton({
   weight: "400",
@@ -8,16 +9,36 @@ const anton = Anton({
   display: "swap",
 });
 
+const zillaSlabHighlight = Zilla_Slab_Highlight({
+  weight: "700",
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function PresentationSection() {
+  const isMobile = useClientMediaQuery("(max-width: 575px)");
+
   return (
     <main className={styles.main}>
-      <Image src={"/basquete-back-home.png"} alt="background" fill={true} />
+      <Image
+        src={
+          isMobile
+            ? "/basquete-back-home-mobile.png"
+            : "/basquete-back-home.png"
+        }
+        alt="background"
+        fill={true}
+      />
       <section className={styles.textBox}>
         <p className={`${styles.firstText} ${anton.className}`}>
           Banana {"Monkey's"}
         </p>
         <p className={`${styles.secondaryText}`}>Descubra a força</p>
       </section>
+
+      <p className={`${zillaSlabHighlight.className} ${styles.mobileText}`}>
+        Uma marca criada especialmente para quem gosta de vencer!
+      </p>
     </main>
   );
 }
