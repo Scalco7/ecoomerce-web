083b79301fa3e1547927f78c15b2a0c7c19f4d65
Scalco7 <fscalco7@gmail.com>
2024-08-15 20:08:24 -0300

    init topSection responsive

--- src/components/organisms/shopPageSections/topSection/topSection.tsx
+++ src/components/organisms/shopPageSections/topSection/topSection.tsx
@@ -1,6 +1,7 @@
 import ProductWithTag from "@/components/molecules/productWithTag/productWithTag";
 import styles from "./style.module.css";
 import { Zilla_Slab } from "next/font/google";
+import { useClientMediaQuery } from "@/hooks/useClientMediaQuery";
 
 const zillaSlab = Zilla_Slab({
   weight: "700",
@@ -9,42 +10,92 @@ const zillaSlab = Zilla_Slab({
 });
 
 export default function TopSection() {
+  const isDesktop = useClientMediaQuery("(min-width: 992px)");
+  const isXLScreen = useClientMediaQuery("(min-width: 1200px)");
+  const isXXLScreen = useClientMediaQuery("(min-width: 1400px)");
+
+  const littleTShirtSize = isXXLScreen
+    ? "420px"
+    : isXLScreen
+    ? "360px"
+    : isDesktop
+    ? "290px"
+    : "225px";
+  const bigTShirtSize = isXXLScreen
+    ? "520px"
+    : isXLScreen
+    ? "450px"
+    : isDesktop
+    ? "370px"
+    : "285px";
+
+  const tagsTshirt1 = isDesktop
+    ? [
+        {
+          price: 89,
+          onCLick: () => {},
+          position: {
+            right: isXXLScreen ? -43 : isXLScreen ? -45 : -50,
+            top: isXXLScreen ? 42 : isXLScreen ? 35 : 23,
+          },
+        },
+      ]
+    : [];
+
+  const tagsTshirt2 = isDesktop
+    ? [
+        {
+          price: 89,
+          onCLick: () => {},
+          position: {
+            right: isXXLScreen ? -3 : isXLScreen ? -17 : -33,
+            top: isXXLScreen ? 106 : isXLScreen ? 90 : 72,
+          },
+        },
+      ]
+    : [];
+
+  const tagsTshirt3 = isDesktop
+    ? [
+        {
+          price: 89,
+          onCLick: () => {},
+          position: {
+            right: isXXLScreen ? -10 : isXLScreen ? -15 : -15,
+            top: isXXLScreen ? 260 : isXLScreen ? 215 : 175,
+          },
+        },
+      ]
+    : [];
+
   return (
     <main className={`${styles.main} ${zillaSlab.className}`}>
-      <section className={styles.txtSection}>
-        <div className={styles.box1}>
-          <p className={styles.text1}>Let the game</p>
-        </div>
-        <div className={styles.box2}>
-          <p className={styles.text2}>Begin</p>
-        </div>
-      </section>
       <section className={styles.imgsSection}>
         <ProductWithTag
           imgSrc={"/t-shirt-1.png"}
-          width={"480px"}
-          height={"480px"}
-          tags={[
-            { price: 89, onCLick: () => {}, position: { right: -18, top: 58 } },
-          ]}
+          width={littleTShirtSize}
+          height={littleTShirtSize}
+          tags={tagsTshirt1}
         />
         <ProductWithTag
           imgSrc={"/t-shirt-2.png"}
-          width={"600px"}
-          height={"600px"}
-          tags={[
-            { price: 89, onCLick: () => {}, position: { right: 8, top: 130 } },
-          ]}
+          width={bigTShirtSize}
+          height={bigTShirtSize}
+          tags={tagsTshirt2}
         />
         <ProductWithTag
           imgSrc={"/t-shirt-3.png"}
-          width={"480px"}
-          height={"480px"}
-          tags={[
-            { price: 89, onCLick: () => {}, position: { right: -3, top: 290 } },
-          ]}
+          width={littleTShirtSize}
+          height={littleTShirtSize}
+          tags={tagsTshirt3}
         />
       </section>
+      <div className={styles.box1}>
+        <p className={styles.text1}>Let the game</p>
+      </div>
+      <div className={styles.box2}>
+        <p className={styles.text2}>Begin</p>
+      </div>
     </main>
   );
 }
