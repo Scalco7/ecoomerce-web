8c80e6be0a46de1fbf0dbc055c3f2498460194e8
Scalco7 <fscalco7@gmail.com>
2024-09-07 20:31:28 -0300

    products section

--- src/app/checkout/cart/page.tsx
+++ src/app/checkout/cart/page.tsx
@@ -4,16 +4,93 @@ import Footer from "@/components/molecules/footer/footer";
 import HeaderNavigation from "@/components/molecules/headerNavigation/headerNavigation";
 import { Toaster } from "react-hot-toast";
 import styles from "./style.module.css";
+import CategorySection from "@/components/organisms/categorySection/categorySection";
+import { buyProduct, productsDataToProductBox } from "@/utils/product.utils";
+import { useProduct, VariantType } from "@/states/productsState";
+import {
+  ModalVariantPromise,
+  onCloseModalVariant,
+  onSelectModalVariant,
+  openModalVariant,
+} from "@/utils/modalVariant.utils";
+import { useCart } from "@/states/cartState";
+import { useState } from "react";
+import ModalVariantSection from "@/components/organisms/modalVariantSection/modalVariantSection";
 
 export default function CheckoutCart() {
+  const { productsSections, styleSection } = useProduct();
+  const { addProduct } = useCart();
+
+  const [variantSectionIsOpen, setVariantSectionIsOpen] = useState(false);
+  const [currentVariant, setCurrentVariant] = useState<
+    VariantType | undefined
+  >();
+  const [resolveModalVariant, setResolveModalVariant] =
+    useState<ModalVariantPromise | null>(null);
+
+  async function handleBuyProduct(productTypeId: string): Promise<void> {
+    await buyProduct(
+      productTypeId,
+      productsSections,
+      addProduct,
+      handleOpenModalVariant
+    );
+  }
+
+  async function handleOpenModalVariant(
+    variantType: VariantType
+  ): Promise<string | undefined> {
+    return openModalVariant(
+      variantType,
+      setVariantSectionIsOpen,
+      setResolveModalVariant,
+      setCurrentVariant
+    );
+  }
+
   return (
     <main className={styles.main}>
       <header className={styles.header}>
         <HeaderNavigation />
       </header>
       <section className={`${styles.body}`}>
+        <section className={styles.buySection}>
+          <CategorySection
+            colorScheme={"yellow"}
+            title={"Aproveite o frete"}
+            products={styleSection.productTypes
+              .slice(0, 4)
+              .map((pt) => productsDataToProductBox(pt))}
+            hasTransiction={true}
+            buyProduct={handleBuyProduct}
+          />
+        </section>
         <Footer />
       </section>
+      {variantSectionIsOpen && (
+        <ModalVariantSection
+          variantBoxWidth={100}
+          variant={currentVariant}
+          onSelect={(variantId) =>
+            onSelectModalVariant(
+              variantId,
+              resolveModalVariant,
+              setVariantSectionIsOpen,
+              setResolveModalVariant,
+              setCurrentVariant
+            )
+          }
+          open={variantSectionIsOpen}
+          onClose={() =>
+            onCloseModalVariant(
+              resolveModalVariant,
+              setVariantSectionIsOpen,
+              setResolveModalVariant,
+              setCurrentVariant
+            )
+          }
+        />
+      )}
       <div>
         <Toaster position="bottom-center" reverseOrder={false} />
       </div>
