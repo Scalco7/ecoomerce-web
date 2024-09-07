06a34d062cef79864dda7e272b09a2e5aaf7a043
Scalco7 <fscalco7@gmail.com>
2024-09-07 20:26:59 -0300

    header checkout

--- src/components/molecules/headerNavigation/headerNavigation.tsx
+++ src/components/molecules/headerNavigation/headerNavigation.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./style.module.css";
 import CartCount from "@/components/atoms/cartCount/cartCount";
-import { Poppins } from "next/font/google";
+import { Poppins, Zilla_Slab } from "next/font/google";
 import { usePathname, useRouter } from "next/navigation";
 
 import HamburguerMenuIcon from "@/components/icons/hamburguerMenuIcon/hamburguerMenuIcon";
@@ -9,9 +9,22 @@ import { useState } from "react";
 import CloseIcon from "@/components/icons/closeIcon/closeIcon";
 import { useCart } from "@/states/cartState";
 import CartSection from "@/components/organisms/cartSection/cartSection";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 const poppins = Poppins({
   weight: ["400", "600"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const zillaSlabBold = Zilla_Slab({
+  weight: "700",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const zillaSlab = Zilla_Slab({
+  weight: "300",
   subsets: ["latin"],
   display: "swap",
 });
@@ -22,6 +35,33 @@ interface INavigationRoute {
   onClick: () => void;
 }
 
+function getRoutes(
+  pathName: string,
+  router: AppRouterInstance
+): INavigationRoute[] {
+  const routes = [
+    {
+      name: "home",
+      selected: false,
+      onClick: () => {},
+    },
+    {
+      name: "shop",
+      selected: false,
+      onClick: () => {},
+    },
+  ];
+
+  routes.forEach((route) => {
+    route.selected = pathName == `/${route.name}`;
+    route.onClick = () => {
+      if (!route.selected) router.push(`/${route.name}`);
+    };
+  });
+
+  return routes;
+}
+
 export default function HeaderNavigation() {
   const [mobileNavigationOpen, setMobileNavigationOpen] = useState(false);
   const [showCart, setShowCart] = useState(false);
@@ -29,31 +69,9 @@ export default function HeaderNavigation() {
   const router = useRouter();
   const { productsQuantity } = useCart();
 
-  const routes = getRoutes();
+  const routes = getRoutes(pathName, router);
 
-  function getRoutes(): INavigationRoute[] {
-    const routes = [
-      {
-        name: "home",
-        selected: false,
-        onClick: () => {},
-      },
-      {
-        name: "shop",
-        selected: false,
-        onClick: () => {},
-      },
-    ];
-
-    routes.forEach((route) => {
-      route.selected = pathName == `/${route.name}`;
-      route.onClick = () => {
-        if (!route.selected) router.push(`/${route.name}`);
-      };
-    });
-
-    return routes;
-  }
+  const isCheckout = pathName.includes("checkout");
 
   const routesComponent = routes.map((route, index) => (
     <p
@@ -88,29 +106,40 @@ export default function HeaderNavigation() {
             />
           </div>
         </section>
-        {mobileNavigationOpen && (
-          <section className={styles.mobileNavBar}>{routesComponent}</section>
+        {isCheckout ? (
+          <p className={zillaSlab.className}>
+            Garantimos que você fez a{" "}
+            <span className={zillaSlabBold.className}>melhor</span> escolha
+          </p>
+        ) : (
+          <>
+            {mobileNavigationOpen && (
+              <section className={styles.mobileNavBar}>
+                {routesComponent}
+              </section>
+            )}
+            <section className={styles.navigationDesktop}>
+              {routesComponent}
+            </section>
+            <section
+              className={`${poppins.className} ${styles.navigationMobile}`}
+              onClick={handleMobileNavBarOpening}
+            >
+              {mobileNavigationOpen ? (
+                <CloseIcon size={30} color="white" />
+              ) : (
+                <HamburguerMenuIcon size={30} strokeWidth={3} color="white" />
+              )}
+            </section>
+            <div style={{ width: "28px" }}>
+              <CartCount
+                quantity={productsQuantity}
+                fontSize={14}
+                onClick={() => toogleShowCart(true)}
+              />
+            </div>
+          </>
         )}
-        <section className={styles.navigationDesktop}>
-          {routesComponent}
-        </section>
-        <section
-          className={`${poppins.className} ${styles.navigationMobile}`}
-          onClick={handleMobileNavBarOpening}
-        >
-          {mobileNavigationOpen ? (
-            <CloseIcon size={30} color="white" />
-          ) : (
-            <HamburguerMenuIcon size={30} strokeWidth={3} color="white" />
-          )}
-        </section>
-        <div style={{ width: "28px" }}>
-          <CartCount
-            quantity={productsQuantity}
-            fontSize={14}
-            onClick={() => toogleShowCart(true)}
-          />
-        </div>
       </main>
       <CartSection open={showCart} closeCart={() => toogleShowCart(false)} />
     </>
